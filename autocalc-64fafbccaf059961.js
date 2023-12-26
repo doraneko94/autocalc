513b8d851946bf4cb2f1bb150104da2d026d4ef8
doraneko94 <shuntaro94@gmail.com>
2023-12-26 20:19:02 +0900

    fix diffeq

--- autocalc-64fafbccaf059961.js
+++ autocalc-64fafbccaf059961.js
@@ -872,7 +872,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('autocalc-23696e95b6f5d0ce_bg.wasm', import.meta.url);
+        input = new URL('autocalc-64fafbccaf059961_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
