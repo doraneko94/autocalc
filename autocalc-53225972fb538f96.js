9a2422c9340f390c0a7a4eb16b932915b757c3c1
doraneko94 <shuntaro94@gmail.com>
2023-12-30 18:08:59 +0900

    before -> after

--- autocalc-53225972fb538f96.js
+++ autocalc-53225972fb538f96.js
@@ -901,7 +901,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('autocalc-249b35406d4ed774_bg.wasm', import.meta.url);
+        input = new URL('autocalc-53225972fb538f96_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
